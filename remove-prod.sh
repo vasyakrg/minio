6d388a2f50821ca6f1a7187715b7cecbe43e29d4
Vassiliy Yegorov <vasyakrg@gmail.com>
2019-12-23 18:04:02 +0700

    fix prod vol

--- remove-prod.sh
+++ remove-prod.sh
@@ -2,4 +2,6 @@
 source .env
 source ./lib/lib.sh
 
+docker volume rm data1-1
+
 remove prod
--- start-prod.sh
+++ start-prod.sh
@@ -2,4 +2,6 @@
 source .env
 source ./lib/lib.sh
 
+docker volume create -o type=ext4 -o device=/dev/sdb --name data1-1
+
 start prod
