8f9012aef4109adfdbf8f3171ddb87d6ca0bac7d
Vassiliy Yegorov <vasyakrg@gmail.com>
2019-12-23 18:26:21 +0700

    change fs

--- start-prod.sh
+++ start-prod.sh
@@ -2,7 +2,7 @@
 source .env
 source ./lib/lib.sh
 
-docker volume create -o type=ext4 -o device=/dev/sdb --name data1-1
-docker volume create -o type=ext4 -o device=/dev/sdc --name data2-1
+docker volume create -o type=xfs -o device=/dev/sdb --name data1-1
+docker volume create -o type=xfs -o device=/dev/sdc --name data2-1
 
 start prod
