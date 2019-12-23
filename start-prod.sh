3c21a995ea15b57c1ba8cb457fef189a00ba624a
Vassiliy Yegorov <vasyakrg@gmail.com>
2019-12-23 20:46:59 +0700

    change prod qty

--- start-prod.sh
+++ start-prod.sh
@@ -2,7 +2,6 @@
 source .env
 source ./lib/lib.sh
 
-docker volume create -o type=xfs -o device=/dev/sdb --name data1-1
-docker volume create -o type=xfs -o device=/dev/sdc --name data2-1
+docker volume create -o type=xfs -o device=/dev/sdb --name data1
 
 start prod
