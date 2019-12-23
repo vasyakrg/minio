d5182aed097e1c5bb43cbf9cc4ec05cfe4a3c698
Vassiliy Yegorov <vasyakrg@gmail.com>
2019-12-23 18:13:02 +0700

    fix disks

--- remove-prod.sh
+++ remove-prod.sh
@@ -5,3 +5,4 @@ source ./lib/lib.sh
 remove prod
 
 docker volume rm data1-1
+docker volume rm data2-1
--- start-prod.sh
+++ start-prod.sh
@@ -2,6 +2,7 @@
 source .env
 source ./lib/lib.sh
 
-docker volume create -o type=ext4 -o device=/dev/sdb --name data1-1
+docker volume create -o type=ext4 -o device=/srv/service/data/disk1 --name data1-1
+docker volume create -o type=ext4 -o device=/srv/service/data/disk2 --name data2-1
 
 start prod
